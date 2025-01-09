eb1040a828ceee60026e9d4a00ab9cad2da262ca
ridham <134303835+rhythm239@users.noreply.github.com>
2025-01-09 22:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Event listeners for buttons
+document.getElementById("catButton").addEventListener("click", function() {
+  const myCat = new Cat("Siamese");
+  document.getElementById("output").innerText = `The ${myCat.species} makes a sound: `;
+  myCat.makeSound();
+  myCat.purr();
+});
+
+document.getElementById("dogButton").addEventListener("click", function() {
+  const myDog = new Dog("Golden Retriever");
+  document.getElementById("output").innerText = `The ${myDog.species} makes a sound: `;
+  myDog.makeSound();
+  myDog.bark();
+});
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
